57bf089436aeb7f8e5de9e4d33b4887a57a1ef17
HELLO <HELLO@swathi>
2021-03-02 17:49:34 +0530

    first commit

---
+++ src/shopping/Echo.java
@@ -0,0 +1,101 @@
+package shopping;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+@Test
+public class Echo {
+	
+	
+	
+	public WebDriver driver;
+
+	@SuppressWarnings("deprecation")
+	@Parameters("browser")
+
+	@BeforeClass
+
+	// Passing Browser parameter from TestNG xml
+
+	public void beforeTest(String browser) {
+
+		// If the browser is Firefox 
+
+		if (browser.equalsIgnoreCase("firefox")) {
+
+			System.setProperty("webdriver.gecko.driver", "C://Drivers/geckodriver.exe");
+
+			// Initialize Gecko Driver using Desired Capabilities Class
+			DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+			capabilities.setCapability("marionette", true);
+			driver = new FirefoxDriver(capabilities);
+
+		} else if (browser.equalsIgnoreCase("chrome")) {
+
+			// Initialize the chrome driver
+
+			System.setProperty("webdriver.chrome.driver", "C://Drivers/chromedriver.exe");
+			driver = new ChromeDriver();
+
+		}
+
+		// Enter the website address in the browser
+
+		driver.get("https://www.amazon.in/");
+
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+	}
+
+	// Once Before method is completed, Test method will start
+
+	public void login() throws InterruptedException {
+
+		// click on hamburger menu
+		WebElement Ele = driver.findElement(By.xpath("//a[@id='nav-hamburger-menu']"));
+		Ele.click();
+		Actions act = new Actions(driver);
+		act.moveToElement(Ele).build().perform();
+		Thread.sleep(7000);
+
+		System.out.println("Clicked on Amazon Menu");
+
+		// Under Digital Content And Devices click on Echo & Alexa
+		driver.findElement(By.xpath("//div[text()='Echo & Alexa']")).click();
+		System.out.println("Under Digital Content And Devices clicked on Echo & Alexa");
+
+		// Under Echo & Alexa click on Echo Dot(3rd Gen)
+		driver.findElement(By.linkText("Echo Dot (3rd Gen)")).click();
+		System.out.println("Under Echo & Alexa clicked on Echo Dot(3rd Gen)");
+
+		// To print items in cart before adding
+		String cartValue = driver.findElement(By.xpath("//span[@id='nav-cart-count']")).getText();
+		System.out.println("Items in cart before adding are " + cartValue);
+
+		// Add to Cart
+		driver.findElement(By.xpath("//input[contains(@id,'add-to-cart-button')]")).click();
+		System.out.println("Items added into cart");
+
+		// To print items in cart after adding
+		String cartValueUpdated = driver.findElement(By.xpath("//span[@id='nav-cart-count']")).getText();
+		System.out.println("Items in cart after adding are " + cartValueUpdated);
+
+	}
+
+	@AfterClass
+	public void afterTest() {
+
+		driver.quit();
+
+	}	
+}
